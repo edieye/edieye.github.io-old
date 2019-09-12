503ec62f40475db1834cc498c32db610637758ed
edieye <e.y.96@hotmail.com>
2019-09-11 17:32:16 -0700

    test

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
-// //variable to hold current angle/degree of rbox
-// var degree = 0;
-// var rbox = document.getElementById('smile');
-// // sets up a rotating smiley in the center
-// var rotatebox = function(){
-//   // add rotation
-//   degree = (degree + 1) % 360;
-//
-//   // add current rotation to the rbox
-//   rbox.style.transform = 'rotate('+degree+'deg)';
-// }
-//
-// // function to hide the smile
-// function hide() {
-//   var smile = document.getElementById("smile");
-//   if (smile.style.display === 'none') {
-//     smile.style.display = 'block';
-//   } else {
-//     smile.style.display = 'none';
-//   }
-// }
-//
-// // sets interval for how often smile rotates
-// setInterval(rotatebox,10);
+//variable to hold current angle/degree of rbox
+var degree = 0;
+var rbox = document.getElementById('smile');
+// sets up a rotating smiley in the center
+var rotatebox = function(){
+  // add rotation
+  degree = (degree + 1) % 360;
+
+  // add current rotation to the rbox
+  rbox.style.transform = 'rotate('+degree+'deg)';
+}
+
+// function to hide the smile
+function hide() {
+  var smile = document.getElementById("smile");
+  if (smile.style.display === 'none') {
+    smile.style.display = 'block';
+  } else {
+    smile.style.display = 'none';
+  }
+}
+
+// sets interval for how often smile rotates
+setInterval(rotatebox,10);
